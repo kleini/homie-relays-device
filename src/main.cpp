59949ec679f9d6ad9fa73bd2ab6de2fc8cda8329
Marcus Klein <himself@kleini.org>
2019-09-26 17:49:05 +0200

    basic Homie device setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #define FW_NAME "homie-heating-control"
-#define FW_VERSION "0.0.1"
+#define FW_VERSION "0.0.2"
 
 #include "Homie.h"
 
+// internal LED of ESP8266-12E/F is connected to GPIO2
+const int PIN_LED = 2;
+
 void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+
+  pinMode(PIN_LED, OUTPUT);
+
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  // Configuration mode removed with compile flags.
+  Homie.disableResetTrigger();
+  Homie.setLedPin(PIN_LED, LOW);
+
   Homie.setup();
 }
 
