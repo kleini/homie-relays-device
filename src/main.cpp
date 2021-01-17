a2717bcfe0477cee3a270b2d699e8f332219541f
Marcus Klein <himself@kleini.org>
2021-01-17 23:14:13 +0100

    feat: add MCP23017 and test it a little bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #define FW_NAME "relays-device"
-#define FW_VERSION "0.0.8"
+#define FW_VERSION "0.0.9"
 
 #include "Homie.h"
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
 
 #include "AdcNode.hpp"
-
-ADC_MODE(ADC_VCC); // Set ADC to measure internal VCC
 
 // internal LED of ESP8266-12E/F is connected to GPIO2
 const int PIN_LED = 2;
 
+Adafruit_MCP23017 mcp;
+
+ADC_MODE(ADC_VCC); // Set ADC to measure internal VCC
 AdcNode adcNode("adc", "Internal", 30 * 1000);
 
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
+
+  mcp.begin();
+  for (int i = 0; i < 16; i++) {
+    mcp.pinMode(i, OUTPUT);
+  }
 
   adcNode.beforeHomieSetup();
 
@@ -29,10 +37,19 @@ void setup() {
 }
 
 unsigned long _lastHeapTime;
+byte leds = 0;
 
 void loop() {
   if (millis() - _lastHeapTime > 30000) {
     Homie.getLogger() << F("Heap: ") << ESP.getFreeHeap() << endl;
+      leds++;
+      for (int i = 0; i < 8; i++) {
+        if ((leds & (1<<i)) != 0) {
+          mcp.digitalWrite(i, HIGH);
+        } else {
+          mcp.digitalWrite(i, LOW);
+        }
+      }
     _lastHeapTime = millis();
   }
   Homie.loop();
